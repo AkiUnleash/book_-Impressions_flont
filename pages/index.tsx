9713d146ba5584759145709407179e2bb76daf87
Aki Unleash <aki.unleash@gmai.comm>
2021-05-20 10:36:17 +0900

    develop initialize.

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Layout from '../components/MyLayout'
+
+export default function Home() {
+  return (
+
+    <Layout>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+        </main>
+      </div>
+    </Layout>
+
+  )
+}
