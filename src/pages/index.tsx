e0c3b0480586b271bb5e9b0cd6ff72439fbea24f
Aki Unleash <aki.unleash@gmai.comm>
2021-05-27 15:51:47 +0900

    Complition a Top screen.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,96 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../assets/styles/Home.module.css'
-import Link from 'next/link'
-import Layout from '../components/templates/MyLayout'
+import Router from 'next/router'
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Home() {
+function Copyright() {
   return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-    <Layout>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-        </main>
-      </div>
-    </Layout>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    margin: theme.spacing(4, 0, 3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    width: 300
+  },
+}));
 
-  )
+export default function SignInSide() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5}  >
+
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5" className={classes.title}>
+            技術書籍感想文（仮）
+          </Typography>
+          <Alert>
+            <AlertTitle>どんなサイト？</AlertTitle>
+            <p>技術書籍の感想文を残すためのサイトです。</p>
+            <p>アウトプットをして知識を深めましょう。</p>
+          </Alert>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => {
+              Router.push('/login')
+            }} >
+            サインイン </Button>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+            onClick={() => {
+              Router.push('/signup')
+            }}
+          >
+            アカウントの登録
+          </Button>
+        </div>
+      </Grid>
+    </Grid>
+  );
 }
